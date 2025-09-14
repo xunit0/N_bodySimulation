b6e6387bfa2894c3ccbff9473a98e2c1ca6ae888
Xavier M <xavier.andre1232@gmail.com>
2025-09-14 19:11:49 -0400

    update

--- N-Body.sh
+++ N-Body.sh
@@ -4,12 +4,11 @@
 #SBATCH --time=02:30:00
 #SBATCH --nodes=1
 #SBATCH --mem=16G
-#SBATCH --output=nbody_%j.out
-#SBATCH --error=nbody_%j.err
+
 
 cd /users/xmolina/work/N_bodySimulation/
 make
 
-/usr/bin/time -v srun ./nbody sem 200 50000 1000
+/usr/bin/time -v srun ./nbody sem 200 5000000 1000
 
 
